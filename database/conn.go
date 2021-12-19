a2411f9e649ff430e518ad69857e4ee7155bda43
fffzlfk <1319933925qq@gmail.com>
2021-12-19 16:10:53 +0800

    aliyun

--- database/conn.go
+++ database/conn.go
@@ -5,20 +5,25 @@ import (
 	"fmt"
 	"log"
 
-	"gorm.io/driver/postgres"
+	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
 var DB *gorm.DB
 
 func Init(host, user, password string) {
-	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s",
-		host,
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:3306)/paste?charset=utf8mb4&parseTime=True&loc=Local", 
 		user,
 		password,
-		"paste",
+		host,
 	)
-	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	// dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s",
+	//	host,
+	//	user,
+	//	password,
+	//	"paste",
+	//)
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatal(err)
 	}
