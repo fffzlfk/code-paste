c8d1a55d41a8dd63e9f99c20af1ef62d66a782a3
fffzlfk <1319933925qq@gmail.com>
2021-12-19 23:23:29 +0800

    Add theme

--- database/conn.go
+++ database/conn.go
@@ -12,17 +12,11 @@ import (
 var DB *gorm.DB
 
 func Init(host, user, password string) {
-	dsn := fmt.Sprintf("%s:%s@tcp(%s:3306)/paste?charset=utf8mb4&parseTime=True&loc=Local", 
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:3306)/paste?charset=utf8mb4&parseTime=True&loc=Local",
 		user,
 		password,
 		host,
 	)
-	// dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s",
-	//	host,
-	//	user,
-	//	password,
-	//	"paste",
-	//)
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatal(err)
