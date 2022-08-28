1101cd41ab71351fc81d2d8dc5166e298f4fdf58
fffzlfk <1319933925qq@gmail.com>
2022-08-28 23:53:37 +0800

    feat(server): add cache

--- server/main.go
+++ server/main.go
@@ -6,8 +6,11 @@ import (
 	"code-paste/database"
 	"fmt"
 	"os"
+	"time"
 
 	"github.com/BurntSushi/toml"
+	"github.com/gin-contrib/cache"
+	"github.com/gin-contrib/cache/persistence"
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
@@ -46,8 +49,10 @@ func main() {
 	corsConfig.AllowAllOrigins = true
 	e.Use(cors.New(corsConfig))
 
+	store := persistence.NewInMemoryStore(time.Second)
+
 	e.POST("/api/create", controllers.CreatePaste)
-	e.GET("/api/read/:id", controllers.ReadPaste)
+	e.GET("/api/read/:id", cache.CachePage(store, time.Minute, controllers.ReadPaste))
 
 	e.Run(fmt.Sprintf(":%d", config.Server.Port))
 }
