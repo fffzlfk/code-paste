162e435a497fd848cfae452a8a46621a33f00301
fffzlfk <44939690+fffzlfk@users.noreply.github.com>
2022-09-03 13:35:24 +0800

    fix: custom api_base_url in docker-compose (#31)

--- web/src/pages/[id].vue
+++ web/src/pages/[id].vue
@@ -9,7 +9,7 @@ let paste = $ref({} as Paste)
 const router = useRouter()
 
 function fetchPaste(id: string) {
-  fetch(`${import.meta.env.VITE_BASE_URL}/read/${id}`)
+  fetch(`${import.meta.env.VITE_API_BASE_URL}/read/${id}`)
     .then(res => res.json())
     .then((res) => {
       paste = res
